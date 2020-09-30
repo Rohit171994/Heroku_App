25c070ba2837bfbce64e9cdbc0d36071509fb595
Rohit171994 <71630355+Rohit171994@users.noreply.github.com>
2020-09-30 20:23:02 +0530

    Add files via upload
    commit for deployment

---
+++ dist/precache-manifest.18243c13cc47f61487f077885c52cd0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d9ee8050f871ca1ddc61af60ae53b60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ac0615f3628a7f791ce",
+    "url": "/static/css/main.fc2a920c.chunk.css"
+  },
+  {
+    "revision": "fe67cfc7a5830c2ec3cc",
+    "url": "/static/js/2.3e05a66c.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.3e05a66c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac0615f3628a7f791ce",
+    "url": "/static/js/main.1ebd7ac9.chunk.js"
+  },
+  {
+    "revision": "5606d481b022dc7ae934",
+    "url": "/static/js/runtime-main.31bed1aa.js"
+  }
+]);
